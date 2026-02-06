d98feb53256a505f456141eb93e6a8eb6247b873
Nasonov Nikita <Cyclist2006@yandex.ru>
2026-02-06 17:05:43 +0300

    02-echo

---
+++ 02-echo/main.c
@@ -0,0 +1,22 @@
+#include "pico/stdlib.h"
+#include "stdio.h"
+#include "hardware/gpio.h"
+
+#define DEVICE_NAME "nosok_pico"
+#define DEVICE_VRSION "v2.2.8"
+
+const uint LED_PIN = 25;
+
+int main()
+{
+    stdio_init_all();
+    while (true)
+    {
+        int c = getchar();
+        if (c != EOF)
+        {
+            char symbol = (char)c;
+            printf("Received char: %c %d\n", symbol, symbol);
+        }
+    }
+}
